d313cc9fa58f99dcc5de4a8385de638f2566d04f
PratikKanani30 <pratik@agnext.in>
2021-06-03 12:05:07 +0530

    annotation added

--- src/main/java/com/agnext/unification/repository/BaseRepository.java
+++ src/main/java/com/agnext/unification/repository/BaseRepository.java
@@ -1,8 +1,9 @@
 package com.agnext.unification.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
-
+@NoRepositoryBean
 public interface BaseRepository<T> extends JpaRepository<T, Long> {
 
   
